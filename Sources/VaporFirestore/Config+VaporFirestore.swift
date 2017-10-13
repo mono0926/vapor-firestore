2f3d8e956d5e2742898385358f37f15b4b5a8388
Masayuki Ono <mono0926@gmail.com>
2017-10-13 20:35:47 +0900

    Add extension for Vapor Config

---
+++ Sources/VaporFirestore/Config+VaporFirestore.swift
@@ -0,0 +1,33 @@
+import Foundation
+import Vapor
+
+extension FireStoreVaporClient: ConfigInitializable {
+    public init(config: Config) throws {
+        guard let firebase = config["firebase"] else {
+            throw ConfigError.missingFile("firebase")
+        }
+        guard let projectId = firebase["projectId"]?.string else {
+            throw ConfigError.missing(key: ["projectId"], file: "firebase", desiredType: String.self)
+        }
+        self = FireStoreVaporClient(projectId: projectId,
+                                    client: try config.resolveClient(),
+                                    logger: try config.resolveLog())
+    }
+}
+
+extension Config {
+    public func addConfigurable<
+        F: FirestoreClient
+        >(firestore: @escaping Config.Lazy<F>, name: String) {
+        customAddConfigurable(closure: firestore, unique: "firestore", name: name)
+    }
+    public func resolveFirestore() throws -> FirestoreClient {
+        return try customResolve(
+            unique: "firestore",
+            file: "firebase",
+            keyPath: ["firestore"],
+            as: FirestoreClient.self,
+            default: FireStoreVaporClient.init
+        )
+    }
+}
